eef27e7a114b92b5822768ef86042a3c087338ef
abhijit826 <abhijitgyan121@gmail.com>
2025-03-30 13:39:21 +0530

    open cv

---
+++ AgeGender.py
@@ -0,0 +1,98 @@
+import cv2 as cv
+import math
+import time
+import argparse
+
+def getFaceBox(net, frame, conf_threshold=0.7):
+    frameOpencvDnn = frame.copy()
+    frameHeight = frameOpencvDnn.shape[0]
+    frameWidth = frameOpencvDnn.shape[1]
+    blob = cv.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+
+    net.setInput(blob)
+    detections = net.forward()
+    bboxes = []
+    for i in range(detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > conf_threshold:
+            x1 = int(detections[0, 0, i, 3] * frameWidth)
+            y1 = int(detections[0, 0, i, 4] * frameHeight)
+            x2 = int(detections[0, 0, i, 5] * frameWidth)
+            y2 = int(detections[0, 0, i, 6] * frameHeight)
+            bboxes.append([x1, y1, x2, y2])
+            cv.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight/150)), 8)
+    return frameOpencvDnn, bboxes
+
+
+parser = argparse.ArgumentParser(description='Use this script to run age and gender recognition using OpenCV.')
+parser.add_argument('--input', help='Path to input image file.', required=True)
+parser.add_argument("--device", default="cpu", help="Device to inference on")
+
+args = parser.parse_args()
+
+faceProto = "opencv_face_detector.pbtxt"
+faceModel = "opencv_face_detector_uint8.pb"
+
+ageProto = "age_deploy.prototxt"
+ageModel = "age_net.caffemodel"
+
+genderProto = "gender_deploy.prototxt"
+genderModel = "gender_net.caffemodel"
+
+MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+genderList = ['Male', 'Female']
+
+# Load network
+ageNet = cv.dnn.readNet(ageModel, ageProto)
+genderNet = cv.dnn.readNet(genderModel, genderProto)
+faceNet = cv.dnn.readNet(faceModel, faceProto)
+
+
+if args.device == "cpu":
+    ageNet.setPreferableBackend(cv.dnn.DNN_TARGET_CPU)
+    genderNet.setPreferableBackend(cv.dnn.DNN_TARGET_CPU)
+    faceNet.setPreferableBackend(cv.dnn.DNN_TARGET_CPU)
+
+    print("Using CPU device")
+elif args.device == "gpu":
+    ageNet.setPreferableBackend(cv.dnn.DNN_BACKEND_CUDA)
+    ageNet.setPreferableTarget(cv.dnn.DNN_TARGET_CUDA)
+    genderNet.setPreferableBackend(cv.dnn.DNN_BACKEND_CUDA)
+    genderNet.setPreferableTarget(cv.dnn.DNN_TARGET_CUDA)
+    print("Using GPU device")
+
+# Load input image
+image = cv.imread(args.input)
+if image is None:
+    print("Image not found, please check the path.")
+    exit()
+
+padding = 20
+# Process the image
+frameFace, bboxes = getFaceBox(faceNet, image)
+if not bboxes:
+    print("No face Detected.")
+else:
+    for bbox in bboxes:
+        face = image[max(0,bbox[1]-padding):min(bbox[3]+padding,image.shape[0]-1),
+                     max(0,bbox[0]-padding):min(bbox[2]+padding, image.shape[1]-1)]
+
+        blob = cv.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+        genderNet.setInput(blob)
+        genderPreds = genderNet.forward()
+        gender = genderList[genderPreds[0].argmax()]
+        print("Gender : {}, conf = {:.3f}".format(gender, genderPreds[0].max()))
+
+        ageNet.setInput(blob)
+        agePreds = ageNet.forward()
+        age = ageList[agePreds[0].argmax()]
+        print("Age : {}, conf = {:.3f}".format(age, agePreds[0].max()))
+
+        label = "{},{}".format(gender, age)
+        cv.putText(frameFace, label, (bbox[0], bbox[1]-10), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv.LINE_AA)
+
+    # Show the result
+    cv.imshow("Age Gender Demo", frameFace)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
